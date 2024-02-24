6df883dddedc11f0bc69a57f726566aab5e93a07
john <jstoppi@gmail.com>
2024-02-24 16:29:29 -0500

    Solution

--- RPS.py
+++ RPS.py
@@ -1,10 +1,35 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
+def player(prev_play, opponent_history=[], play_order={}):
+    if not prev_play:
+        prev_play = 'R'
 
-def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    # Default prediction adjusts to be less predictable against Abbey
+    prediction = 'P' if len(opponent_history) % 2 == 0 else 'S'
 
-    return guess
+    # Extend pattern analysis for Abbey
+    sequence_length = 5 if len(opponent_history) % 5 == 0 else 4  # Disrupt pattern length predictably
+
+    if len(opponent_history) > sequence_length:
+        last_sequence = "".join(opponent_history[-sequence_length:])
+        play_order[last_sequence] = play_order.get(last_sequence, 0) + 1
+
+        potential_next_plays = [
+            "".join([*opponent_history[-(sequence_length - 1):], v])
+            for v in ['R', 'P', 'S']
+        ]
+
+        sub_order = {
+            sequence: play_order[sequence]
+            for sequence in potential_next_plays if sequence in play_order
+        }
+
+        if sub_order:
+            prediction = max(sub_order, key=sub_order.get)[-1:]
+        else:
+            # Use a randomized approach when no pattern is detected
+            prediction = random.choice(['R', 'P', 'S'])
+
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    return ideal_response[prediction]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This entrypoint file to be used in development. Start by reading README.md
 from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
+import random
 from unittest import main
 
 play(player, quincy, 1000)
